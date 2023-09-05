4bca858af73b60aeeb91aa3ba9b94a6c0443b27b
kimemia <david.thuku@student.moringaschool.com>
2023-09-05 03:56:00 -0700

    Done with scheming

--- lib/sqlalchemy_sandbox.py
+++ lib/sqlalchemy_sandbox.py
@@ -1,12 +1,31 @@
 #!/usr/bin/env python3
 
+
+
 from sqlalchemy import Column, Integer, String, create_engine
 from sqlalchemy.ext.declarative import declarative_base
 
+
+
+
 Base = declarative_base()
 
+
+
+
 class Student(Base):
-    pass
+    __tablename__= 'students'
+    id=Column(Integer(),primary_key=True)
+    name=Column(String())
+    
+    # this is a model class 
 
 if __name__ == '__main__':
-    pass
+    # data  models
+
+    engine=create_engine("sqlite:///students.db")
+    try:
+        Base.metadata.create_all(engine)
+        print("Table was created Successfully",Student.__tablename__)
+    except Exception as e:
+        print("an error occured:",str(e))    
